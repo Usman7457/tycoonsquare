9982b41fe179f1fcb8674d5667370375f37076ac
Usman Sheikh <149481505+Usman7457@users.noreply.github.com>
2023-11-14 15:39:17 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home(props) {
   return (
     <div id="hero" className="d-flex align-items-center">
     <div className="container position-relative"> 
       <h1 style={{fontWeight: '75px'}}>{props.heading}</h1>
-      <a href="/contact" style={{marginTop: '10px'}} className="btn-get-started scrollto">{props.buttonTitle}</a>
+      <Link to="/contact" style={{marginTop: '10px'}} className="btn-get-started scrollto">{props.buttonTitle}</Link>
     </div>
   </div>
   )
